d2fafe84e043cf01d842defec9e854796c57ab9e
mstarace <mstarace@POM-NJ-002>
2023-09-08 15:27:26 -0400

    iOS Custom Picker Renderer -Added a custom picker renderer for iOS -The renderer customizes the way that items are displayed inthe picker wheel, and displays the title of the picker on it's UIToolbar

--- SimplePressureRegulator/SimplePressureRegulator.iOS/CustomPickerRenderer.cs
+++ SimplePressureRegulator/SimplePressureRegulator.iOS/CustomPickerRenderer.cs
@@ -1,6 +1,7 @@
 ﻿using Foundation;
 using SimplePressureRegulator.Controls;
 using SimplePressureRegulator.iOS;
+using SpriteKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,20 +13,71 @@ using Xamarin.Forms.Platform.iOS;
 [assembly: ExportRenderer(typeof(CustomPicker), typeof(CustomPickerRenderer))]
 namespace SimplePressureRegulator.iOS
 {
-    public class CustomPickerRenderer : PickerRenderer
+    public class CustomPickerRenderer : PickerRenderer, IUIPickerViewDelegate, IUIPickerViewDataSource
     {
+        int SelectedIndex = -1;
         protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
         {
             base.OnElementChanged(e);
-
             var element = (CustomPicker)this.Element;
-
-            if (this.Control != null && this.Element != null && !string.IsNullOrEmpty(element.Image))
+            if (Control != null)
             {
-                var downarrow = UIImage.FromBundle(element.Image);
-                Control.RightViewMode = UITextFieldViewMode.Always;
-                Control.RightView = new UIImageView(downarrow);
+                element.SelectedIndex = SelectedIndex;
+
+                UIPickerView pickerView = (UIPickerView)Control.InputView;
+                pickerView.WeakDelegate = this;
+                pickerView.DataSource = this;
+
+                UIToolbar toolbar = (UIToolbar)Control.InputAccessoryView;
+                UIBarButtonItem doneBtn = new UIBarButtonItem("Done", UIBarButtonItemStyle.Done, (object sender, EventArgs click) =>
+                {
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = 0;
+                    }
+                    element.SelectedIndex = SelectedIndex;
+                    toolbar.RemoveFromSuperview();
+                    pickerView.RemoveFromSuperview();
+                    Control.ResignFirstResponder();
+                });
+                var doneBtnTextAttributes = new UITextAttributes
+                {
+                    TextColor = UIColor.Black
+                };
+                UIBarButtonItem pickerTitle = new UIBarButtonItem(element.Title, UIBarButtonItemStyle.Done, null);
+                pickerTitle.SetTitleTextAttributes(doneBtnTextAttributes, UIControlState.Normal);
+                UIBarButtonItem empty = new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace);
+                toolbar.Items = new UIBarButtonItem[] { empty, pickerTitle, empty, doneBtn };
             }
+        }
+
+        public nint GetComponentCount(UIPickerView pickerView)
+        {
+            return 1;
+        }
+        public nint GetRowsInComponent(UIPickerView pickerView, nint component)
+        {
+            return Element.Items.Count;
+        }
+
+        [Export("pickerView:viewForRow:forComponent:reusingView:")]
+        public UIView GetView(UIPickerView pickerView, nint row, nint component, UIView view)
+        {
+            UILabel label = new UILabel
+            {
+                TextColor = UIColor.Black,
+                Text = Element.Items[(int)row].ToString(),
+                TextAlignment = UITextAlignment.Center,
+            };
+            return label;
+        }
+
+        [Export("pickerView:didSelectRow:inComponent:")]
+        public void Selected(UIPickerView pickerView, nint row, nint component)
+        {
+            var element = (CustomPicker)this.Element;
+            SelectedIndex = (int)row;
+            element.SelectedIndex = SelectedIndex;
         }
     }
 }
